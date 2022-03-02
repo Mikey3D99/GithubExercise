84acaef5976d97297c5f888b23805e0d7abf3785
Michał Włodarczyk <michalwlodarczykbusiness@gmail.com>
2022-03-02 15:25:33 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/GithubExercise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GithubExercise.dir/main.cpp.obj"
+  "GithubExercise.exe"
+  "GithubExercise.exe.manifest"
+  "GithubExercise.pdb"
+  "libGithubExercise.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GithubExercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
